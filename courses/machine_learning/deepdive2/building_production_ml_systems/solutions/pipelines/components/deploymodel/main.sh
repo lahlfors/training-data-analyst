bc26de09c38c732bc9082d8b5041d0c750069183
lahlfors <ahlfors.lars@gmail.com>
2021-03-10 13:34:21 +0000

    fixed deployment

--- courses/machine_learning/deepdive2/building_production_ml_systems/solutions/pipelines/components/deploymodel/main.sh
+++ courses/machine_learning/deepdive2/building_production_ml_systems/solutions/pipelines/components/deploymodel/main.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 
-BUCKET="$1"
-
-test -z "$BUCKET" && {
-  echo "Usage: $(basename $0) <BUCKET>"
-  exit 1
-}
-
-REGION=europe-west1
+BUCKET=laah-buzz-bucket
+REGION=us-central1
 MODEL_NAME=taxifare
 VERSION_NAME=dnn
 
@@ -18,18 +12,18 @@ PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 EXPORT_PATH=$(gsutil ls $OUTPUT_DIR/export/savedmodel | tail -1)
 
 
-if [[ $(gcloud ai-platform models list --format='value(name)' | grep $MODEL_NAME) ]]; then
-    echo "$MODEL_NAME already exists"
-else
-    echo "Creating $MODEL_NAME"
-    gcloud ai-platform models create --regions=$REGION $MODEL_NAME
-fi
+#if [[ $(gcloud ai-platform models list --region=$REGION --format='value(name)' | grep $MODEL_NAME) ]]; then
+#    echo "$MODEL_NAME already exists"
+#else
+echo "Creating $MODEL_NAME"
+gcloud ai-platform models create --region=$REGION $MODEL_NAME
+#fi
 
-if [[ $(gcloud ai-platform versions list --model $MODEL_NAME --format='value(name)' | grep $VERSION_NAME) ]]; then
-    echo "Deleting already existing $MODEL_NAME:$VERSION_NAME ... "
-    echo yes | gcloud ai-platform versions delete --model=$MODEL_NAME $VERSION_NAME
-    sleep 2
-fi
+#if [[ $(gcloud ai-platform versions list --model $MODEL_NAME --region $REGION --format='value(name)' | grep $VERSION_NAME) ]]; then
+#    echo "Deleting already existing $MODEL_NAME:$VERSION_NAME ... "
+#    echo yes | gcloud ai-platform versions delete --model=$MODEL_NAME $VERSION_NAME
+#    sleep 2
+#fi
 
 echo "Creating $MODEL_NAME:$VERSION_NAME"
 # --async \
